d85568137f98c4531002f4bf4bbc95e9eeee2b07
Mateusz Grzanka <mateusz.grzanka@kcl.ac.uk>
2022-10-24 15:32:40 +0100

    Add validation constraints to name, description and quantity fields

--- things/models.py
+++ things/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(
+        max_length = 30,
+        unique = True,
+        blank = False
+        )
+    description = models.CharField(
+        max_length = 120,
+        blank = True
+    )
+    quantity = models.IntegerField(
+        validators = [
+            MaxValueValidator(
+                limit_value = 100
+            ),
+            MinValueValidator(
+                limit_value = 0
+            )
+        ]
+    )
